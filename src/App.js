04c0ced2f4ec2a9eeaa876e418a1d1e4439fa027
Vikrant Reddy <vikrantreddy@hotmail.com>
2021-10-08 12:23:23 +0530

    Contact section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Work from "./components/Work.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <About />
       <Work />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Container, Row, Button } from 'react-bootstrap'
+import styled, { css } from 'styled-components'
+
+function Contact() {
+    return (
+        <ContactContainer>
+            <h2 style={{ "text-align": "center" }}>Contact Me</h2>
+            <Row className="justify-content-center">
+                <HeroButton primary>Form</HeroButton>
+                <HeroButton>Email</HeroButton>
+            </Row>
+        </ContactContainer >
+    )
+}
+
+export default Contact
+
+const ContactContainer = styled(Container)`
+    background-color:#474C4C;
+    color:#f2f2f2;
+    font-family: 'Open Sans', sans-serif;
+    padding:5%;
+    margin:2%;
+    max-width: 96%;
+`
+const HeroButton = styled(Button)`
+  background: transparent;
+  font-size:larger;
+  border-radius: 3px;
+  font-family: "Montserrat", sans-serif;
+  border: 2px solid #5692EB;
+  color: #5692EB;
+  margin: 1em;
+  padding: 0.25em 1.5em;
+
+  ${props => props.primary && css`
+    background: #5692EB;
+    color: #f2f2f2;
+  `}
+`;
