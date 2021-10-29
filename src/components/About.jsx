55292d13db4f9c587486e384322934d21cfb820a
Vikrant Reddy <vikrantreddy@hotmail.com>
2021-10-29 18:48:00 +0530

    Added Flip card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import styled from 'styled-components'
-
+import ReactCardFlip from 'react-card-flip';
 function About() {
+
+    const [isFlipped1, flip1] = useState(false)
+    const [isFlipped2, flip2] = useState(false)
+
+    function handleClick1(e) {
+        e.preventDefault();
+        flip1(!isFlipped1)
+    }
+
+    function handleClick2(e) {
+        e.preventDefault();
+        flip2(!isFlipped2)
+    }
+
     return (
         <AboutContainer>
             <h2 style={{ "textAlign": "center" }}>About Me</h2>
             <Row style={{ "margin": "10% 0" }}>
-                <Col md={{ span: 3, offset: 1 }}>
-                    <AboutText>
-                        One day, I figured out that I could make my pc do my work
-                    </AboutText>
+                <Col>
+                    <ReactCardFlip isFlipped={isFlipped1} flipDirection="horizontal">
+                        <AboutRow onClick={handleClick1}>
+                            <Col>
+                                Personal.
+                            </Col>
+                        </AboutRow>
+
+                        <AboutRow onClick={handleClick1}>
+                            This is the back of the card.
+                        </AboutRow>
+                    </ReactCardFlip>
                 </Col>
 
-                <Col md={{ span: 3, offset: 4 }}>
-                    <AboutText>
-                        And ever since, my life hasn't been the same
-                    </AboutText>
+                <Col>
+                    <ReactCardFlip isFlipped={isFlipped2} flipDirection="horizontal">
+                        <AboutRow onClick={handleClick2}>
+                            Professional.
+                        </AboutRow>
+
+                        <AboutRow onClick={handleClick2}>
+                            This is the back of the card.
+                        </AboutRow>
+                    </ReactCardFlip>
                 </Col>
             </Row>
         </AboutContainer >
@@ -37,4 +65,10 @@ const AboutText = styled.p`
     font-family: 'Open Sans', sans-serif;
     font-size: larger;
     text-align: center;
+`
+const AboutRow = styled(Col)`
+    border: 1px solid;
+    padding: 1rem;
+    margin: 0 2vw;
+    max-width:80%
 `
