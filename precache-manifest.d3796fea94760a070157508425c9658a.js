2ad66bc87aa5af674263de81a1a920ccc1f7e4c1
Vikrant Reddy <vikrantreddy@hotmail.com>
2021-10-29 16:57:46 +0530

    Updates

---
+++ precache-manifest.d3796fea94760a070157508425c9658a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc53130243894eb204d645f6841b02d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ead7bb3cd0bf76c378f",
+    "url": "/static/js/2.3025a1ed.chunk.js"
+  },
+  {
+    "revision": "53f45c27f7e2f552fe9ddf7c7d95a8d1",
+    "url": "/static/js/2.3025a1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf97ee1c6ebd55c7fe4",
+    "url": "/static/js/main.81c69c9f.chunk.js"
+  },
+  {
+    "revision": "586db9978acfdb77e7d2",
+    "url": "/static/js/runtime-main.a1d8af81.js"
+  },
+  {
+    "revision": "7b861fe6dbd8775212eba62633181c90",
+    "url": "/static/media/herosvg.7b861fe6.svg"
+  },
+  {
+    "revision": "c18343c1f889ffe452489ec45e2432d6",
+    "url": "/static/media/logo.c18343c1.svg"
+  }
+]);
